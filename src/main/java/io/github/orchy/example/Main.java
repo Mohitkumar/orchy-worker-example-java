2a4282b6a9c8ca0e24d2063d161541249556dd97
mohitkumar <mohit@freightexchange.com.au>
2023-01-01 13:22:55 +0530

    example

---
+++ src/main/java/io/github/orchy/example/Main.java
@@ -0,0 +1,42 @@
+package io.github.orchy.example;
+
+import io.github.mohitkumar.orchy.worker.Worker;
+import io.github.mohitkumar.orchy.worker.WorkerManager;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+        Function<Map<String, Object>, Map<String, Object>> addDataFn= (Map<String, Object> input) ->{
+            System.out.println(input);
+            input.put("newKey",22);
+            return input;
+        };
+        Function<Map<String, Object>, Map<String, Object>> printFn= (Map<String, Object> input) ->{
+            System.out.println(input);
+            return input;
+        };
+        Function<Map<String, Object>, Map<String, Object>> enhanceDataFn= (Map<String, Object> input) ->{
+            input.put("key", "prefix_"+input.get("key"));
+            return input;
+        };
+        Worker worker1 = Worker
+                .newBuilder()
+                .DefaultWorker(addDataFn,"add-data-worker", 2,1, TimeUnit.MILLISECONDS).withTimeout(80).
+                build();
+        Worker worker2 = Worker
+                .newBuilder()
+                .DefaultWorker(printFn,"print-worker", 2,1,TimeUnit.MILLISECONDS).build();
+
+        Worker worker3 = Worker
+                .newBuilder()
+                .DefaultWorker(enhanceDataFn,"enhanceData", 2,1,TimeUnit.MILLISECONDS).build();
+        WorkerManager manager = new WorkerManager("localhost",8099);
+        manager.registerWorker(worker1,10);
+        manager.registerWorker(worker2,10);
+        manager.registerWorker(worker3,10);
+        manager.start();
+    }
+}
